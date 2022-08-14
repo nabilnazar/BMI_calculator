cca66190b4bb931ddf14d1d577b3764e22a19d9a
nabilnazarp <nabilnazarp@gmail.com>
2022-08-14 07:52:02 +0300

    Initial commit

---
+++ app/src/main/java/com/meghamlabs/bmi_calculator/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.meghamlabs.bmi_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btnCalculate)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+
+            val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
+            // get result with two decimal places
+            val bmi2Digits = String.format("%.2f", bmi).toFloat()
+            displayResult(bmi2Digits)
+        }
+
+    }
+
+    private fun displayResult(bmi: Float) {
+        val resultIndex = findViewById<TextView>(R.id.tvIndex)
+        val resultDescription = findViewById<TextView>(R.id.tvResult)
+        val info = findViewById<TextView>(R.id.tvInfo)
+
+        resultIndex.text = bmi.toString()
+        info.text = "(Normal range is 18.5 - 24.9 )"
+
+        var resultText = ""
+        var color = 0
+
+        when{
+            bmi<18.50 ->{
+                resultText = "Underweight"
+                color = R.color.under_weight
+            }
+            bmi in 18.50..24.99->{
+                resultText = "Healthy"
+                color =R.color.normal
+            }
+            bmi in 25.00..29.99->{
+                resultText = "Overweight"
+                color = R.color.over_weight
+            }
+            bmi > 29.99 -> {
+                resultText ="Obese"
+                color = R.color.obese
+            }
+
+        }
+        resultDescription.setTextColor(ContextCompat.getColor(this,color))
+        resultDescription.text = resultText
+    }
+}
